e4f6ec27f2e3ffec2e56d9d9c86db8ca261b0d43
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-06 19:11:53 -0600

    completed user class and corrected application.yml values

--- src/main/java/com/dreamsw/security/user/User.java
+++ src/main/java/com/dreamsw/security/user/User.java
@@ -1,5 +1,9 @@
 package com.dreamsw.security.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,8 +18,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "_user")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
     private String firstname;
     private String lastname;
