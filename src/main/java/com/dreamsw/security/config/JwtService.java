6ade0d5c90bea06da069e112e915cdee9c93726e
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-08 16:42:36 -0600

    added jwt service methoids

--- src/main/java/com/dreamsw/security/config/JwtService.java
+++ src/main/java/com/dreamsw/security/config/JwtService.java
@@ -1,6 +1,12 @@
 package com.dreamsw.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 /**
  * @author Daniel
@@ -10,8 +16,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class JwtService {
 
+    // secret key generated in https://allkeysgenerator.com/Random/Security-Encryption-Key-Generator.aspx
+    private static final String SECRET_KEY = "4B6150645367566B59703373357638792F423F4528482B4D6251655468576D5A";
 
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
