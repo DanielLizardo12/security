2ba28f50a894df902704263347ff062798620a1a
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-08 17:07:12 -0600

    added applitacion config

--- src/main/java/com/dreamsw/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/dreamsw/security/config/JwtAuthenticationFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -21,6 +24,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -43,5 +47,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // The jwt token will start after "Bearer ", so it will start at index 7
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
+
+        // validate if userEmail is not null and also not already authenticated
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+        }
     }
 }
