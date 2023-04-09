48d6cecac4da139cd101ed6a6fb79a849a653e65
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-09 17:20:57 -0600

    implemented register method

--- src/main/java/com/dreamsw/security/auth/AuthenticationService.java
+++ src/main/java/com/dreamsw/security/auth/AuthenticationService.java
@@ -1,6 +1,11 @@
 package com.dreamsw.security.auth;
 
+import com.dreamsw.security.config.JwtService;
+import com.dreamsw.security.user.Role;
+import com.dreamsw.security.user.User;
+import com.dreamsw.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,7 +16,26 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstName())
+                .lastname(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
