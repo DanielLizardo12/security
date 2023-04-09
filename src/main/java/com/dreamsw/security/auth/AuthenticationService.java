6dab9994e53b7dcbff8ae6a71eb9e1d58bf2ef72
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-09 17:16:14 -0600

    added authentication service

---
+++ src/main/java/com/dreamsw/security/auth/AuthenticationService.java
@@ -0,0 +1,19 @@
+package com.dreamsw.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Daniel
+ * @Date 09/04/2023
+ */
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    public AuthenticationResponse register(RegisterRequest request) {
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+    }
+}
